fe0e855e8a23078fe08b78fc6d18f6aceffb2a3f
Your Name <987357044@qq.com>
2017-09-04 19:38:21 +0800

    python 实例1

---
+++ 实例/即时标记/util.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# 文本块生成器
+def lines(file):
+    for line in file:yield line
+    yield '\n'
+# 找出段落
+def blocks(file):
+    block = []
+    for line in lines(file):
+        if line.strip():
+            block.append(line)
+        elif block:
+            yield "".join(block).strip()
+            block = []
